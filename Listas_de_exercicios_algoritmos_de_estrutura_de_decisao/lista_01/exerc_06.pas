ffddc447b69b9faf90092ecdf499e4daa6a8cd6a
unknown <augustolenhart@gmail.com>
2019-09-16 17:49:03 -0300

    Adição do exercicio 6

---
+++ Listas_de_exercicios_algoritmos_de_estrutura_de_decisao/lista_01/exerc_06.pas
@@ -0,0 +1,24 @@
+{Crie um algoritmo para uma empresa de transportes que, a partir do peso de uma
+encomenda fornecida pelo usuário, calcule o preço da remessa conforme a
+seguinte tabela:
+Peso da encomenda Valor
+500 gramas ou menos R$ 1,10
+Mais de 500 gramas, mas não mais que
+2 quilos
+R$ 2,20
+Mais de 2 quilos, mas não mais de 10
+quilos
+R$ 3,70
+Mais de 10 quilos R$ 5,00 mais R$ 3,80/kg pelo peso que ultrapassar 10 Kg}
+program exerc_06;
+uses crt;
+  var
+    peso, remessa :real;
+  begin
+    writeln('Informe o peso da encomenda: ')
+    read(peso);
+    if peso = 500 then
+      begin
+        writeln('Valor da remessa eh R$ 1,10');
+      end;
+  end.
